1d32271db4711ce99ed49e27887e1aaa778ab00e
Alex Sander Resende de Deus <prof.alexresende@gmail.com>
2022-03-03 17:47:41 -0300

    Aula Cards

---
+++ app/src/main/java/com/example/kotlin_material_design_aula04_cards/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_material_design_aula04_cards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
